ec2830ff015cf00a73ec2030969772f63d0d1f6a
DragonLord146 <dragonlord851042@gmail.com>
2026-04-01 17:46:57 +0100

    started speed boost power up (1/4/2026)

---
+++ Assets/Scripts/Power Ups/SpeedBoost.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SpeedBoost : MonoBehaviour
+{
+    public Player_Movement playerMovement;
+
+    private void OnTriggerEnter2D()
+    {
+        //playerMovement.int runSpeed * 3f;
+    }
+}
